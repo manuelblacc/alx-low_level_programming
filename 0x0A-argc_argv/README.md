072bca4fb41f0151371098192f5a29b6dc82e848
manuelblacc <emmanuelisikakuc@gmail.com>
2022-11-07 21:21:44 -0800

    Task 1

--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 File: 0- a program that prints its name
+File: 1- a program that prints the number of arguments passed into it
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,5 @@
-#ifndef __MAIN.H__
-#define __MAIN.H__
+#ifndef _MAIN_
+#define _MAIN_
 int _putchar(char c);
 int is_integer(const char *str);
 int get_coin_number(int amount, int nb_coin);
