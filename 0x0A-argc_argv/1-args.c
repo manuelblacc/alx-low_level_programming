6fb26b4372446be92516f63c284b0d45a262892b
manuelblacc <emmanuelisikakuc@gmail.com>
2022-11-09 16:44:44 -0800

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,14 +3,15 @@
 
 /**
  * main - programs that prints the number of arguments passed into it
- * @argc - size of argv
- * @argv - array containing the program line arguments
+ * @argc: size of argv
+ * @argv: array containing the program line arguments
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	(void) argv;
-	printf("%d\n", argc - 1);
+(void) argv;
+printf("%i\n", argc - 1);
 
-	return (0);
+return (0);
 }
